d9871ac701eafbed52bf5bb6a941a0048ddd3339
jmaire <julien.maire.pro@gmail.com>
2025-03-13 02:10:26 -0700

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -18,20 +18,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "toolios",
-            targets: ["toolios"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            targets: ["toolios"],
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "toolios",
-            dependencies: []),
+            path: "Sources",
         ),
         .testTarget(
             name: "toolios_Tests",
-            dependencies: ["toolios"]),
+            dependencies: ["toolios"],
+        ),
     ]
 )
